3190369ae0fbbd9abb5ed0006526ced929df2a73
it-school <GITHUB it@osvita.od.ua>
2020-10-03 14:03:20 +0300

    v.1

--- src/com/itschool/Main.java
+++ src/com/itschool/Main.java
@@ -14,7 +14,7 @@ public class Main
 		db.add(new Student("Null", 2100000000));
 		// System.out.println(db);
 
-		long t1 = System.currentTimeMillis(), t2, t3, t4, t5;
+		long t1 = System.currentTimeMillis(), t2, t3, t4, t5, t6;
 		for (int i = 0; i < 10; i++) {
 			db.add("John" + Math.random(), (int) (Math.random() * 100));
 		}
@@ -39,16 +39,19 @@ public class Main
 		db.jacksonDeserialize("students.json");
 		t5 = System.currentTimeMillis() - t4 - t3 - t2 - t1;
 
-		System.out.println("Generation    time:" + t2);
-		System.out.println("Save/load     time:" + t3);
-		System.out.println("Serialization time:" + t4);
-		System.out.println("Jackson s/d   time:" + t5);
 		// System.out.println(db);
 
 		db.serializeFastJSON("db_fastjson.txt");
 		db.clear();
 		db.deserializeFastJSON("db_fastjson.txt");
-		System.out.println("Loaded fastJSON deseriaized DB: " + db);
+//		System.out.println("Loaded fastJSON deseriaized DB: " + db);
+		t6 = System.currentTimeMillis() - t5 - t4 - t3 - t2 - t1;
+
+		System.out.println("Data Generation time:    " + t2);
+		System.out.println("Text Save/load  time:    " + t3);
+		System.out.println("Java serialization time: " + t4);
+		System.out.println("Jackson ser/des time:    " + t5);
+		System.out.println("FASTJson ser/des time:   " + t6);
 /*
         System.out.println(db.remove(1));
         System.out.println(db);*/
