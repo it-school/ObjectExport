9c38949286eeea909b59e67b8b60d9bba17f4dc7
it-school <GITHUB it@osvita.od.ua>
2020-10-03 14:35:02 +0300

    Syntax changes

--- src/com/itschool/Database.java
+++ src/com/itschool/Database.java
@@ -9,144 +9,121 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-public class Database
-{
-	public ArrayList<Student> list;
+public class Database {
+    public ArrayList<Student> list;
 
-	public Database()
-	{
-		list = new ArrayList<>();
-	}
+    public Database() {
+        list = new ArrayList<>();
+    }
 
-	public void add(String name, int age)
-	{
-		this.list.add(new Student(name, age));
-	}
+    public void add(String name, int age) {
+        this.list.add(new Student(name, age));
+    }
 
-	public Student get(int index)
-	{
-		return this.list.get(index);
-	}
+    public Student get(int index) {
+        return this.list.get(index);
+    }
 
-	public Student remove(int index)
-	{
-		return this.list.remove(index);
-	}
+    public Student remove(int index) {
+        return this.list.remove(index);
+    }
 
-	@Override
-	public String toString()
-	{
-		return "Database{" + list + '}';
-	}
+    @Override
+    public String toString() {
+        return "Database{" + list + '}';
+    }
 
-	public void save(String filename) throws IOException
-	{
-		FileWriter outStream = new FileWriter(filename);
-		BufferedWriter bw = new BufferedWriter(outStream);
-		for (Student student : list) {
-			try {
-				bw.write(student.name);
-				bw.write(System.lineSeparator());
-				bw.write(String.valueOf(student.age));
-				bw.write(System.lineSeparator());
-			}
-			catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+    public void save(String filename) throws IOException {
+        FileWriter outStream = new FileWriter(filename);
+        BufferedWriter bw = new BufferedWriter(outStream);
+        for (Student student : list) {
+            try {
+                bw.write(student.name);
+                bw.write(System.lineSeparator());
+                bw.write(String.valueOf(student.age));
+                bw.write(System.lineSeparator());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
 
-		bw.close();
-		outStream.close();
-	}
+        bw.close();
+        outStream.close();
+    }
 
-	public void load(String filename) throws IOException
-	{
-		this.clear();
-		//this.list = new ArrayList<>();
-		Scanner scanner = new Scanner(new FileReader(filename));
-		String name = "";
-		int age = -1;
-		while (scanner.hasNextLine()) {
-			name = scanner.nextLine();
-			age = Integer.valueOf(scanner.nextLine());
-			this.list.add(new Student(name, age));
-		}
-		scanner.close();
-	}
+    public void load(String filename) throws IOException {
+        this.clear();
+        //this.list = new ArrayList<>();
+        Scanner scanner = new Scanner(new FileReader(filename));
+        String name = "";
+        int age = -1;
+        while (scanner.hasNextLine()) {
+            name = scanner.nextLine();
+            age = Integer.parseInt(scanner.nextLine());
+            this.list.add(new Student(name, age));
+        }
+        scanner.close();
+    }
 
-	public void clear()
-	{
-		this.list.clear();
-	}
+    public void clear() {
+        this.list.clear();
+    }
 
-	public void serialize(String filename)
-	{
-		try {
-			FileOutputStream fileOut = new FileOutputStream(filename);
-			ObjectOutputStream out = new ObjectOutputStream(fileOut);
-			out.writeObject(this.list);
-			out.close();
-			fileOut.close();
-		}
-		catch (IOException ex) {
-			ex.printStackTrace();
-		}
-	}
+    public void serialize(String filename) {
+        try {
+            FileOutputStream fileOut = new FileOutputStream(filename);
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(this.list);
+            out.close();
+            fileOut.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
-	public void deserialize(String filename)
-	{
-		try {
-			FileInputStream fileIn = new FileInputStream(filename);
-			ObjectInputStream in = new ObjectInputStream(fileIn);
-			this.list = (ArrayList<Student>) in.readObject();
-			in.close();
-			fileIn.close();
-		}
-		catch (IOException ex) {
-			ex.printStackTrace();
-			return;
-		}
-		catch (ClassNotFoundException c) {
-			System.out.println("Student class not found");
-			c.printStackTrace();
-			return;
-		}
-	}
+    public void deserialize(String filename) {
+        try {
+            FileInputStream fileIn = new FileInputStream(filename);
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            this.list = (ArrayList<Student>) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("Student class not found");
+            c.printStackTrace();
+        }
+    }
 
-	public void jacksonSerialize(String filename) throws IOException
-	{
-		new ObjectMapper().writeValue(new File(filename), this);
-	}
+    public void jacksonSerialize(String filename) throws IOException {
+        new ObjectMapper().writeValue(new File(filename), this);
+    }
 
-	public void jacksonDeserialize(String filename) throws IOException
-	{
-		Database db1 = new ObjectMapper().readValue(new File(filename), Database.class);
-		this.list = db1.list;
-	}
+    public void jacksonDeserialize(String filename) throws IOException {
+        Database db1 = new ObjectMapper().readValue(new File(filename), Database.class);
+        this.list = db1.list;
+    }
 
-	public void serializeFastJSON(String filename) throws IOException
-	{
-		FileWriter outStream = new FileWriter(filename);
-		BufferedWriter bw = new BufferedWriter(outStream);
-		bw.write(JSON.toJSONString(this.list));
-		bw.close();
-		outStream.close();
-	}
+    public void serializeFastJSON(String filename) throws IOException {
+        FileWriter outStream = new FileWriter(filename);
+        BufferedWriter bw = new BufferedWriter(outStream);
+        bw.write(JSON.toJSONString(this.list));
+        bw.close();
+        outStream.close();
+    }
 
-	public void deserializeFastJSON(String filename) throws IOException
-	{
-		Scanner scanner = new Scanner(new FileReader(filename));
-		this.clear();
-		Object object = JSON.parseObject(scanner.nextLine(), ArrayList.class);
-		for (JSONObject st : (ArrayList<JSONObject>) object) {
-			this.add(new Student(st.getString("name"), st.getIntValue("age")));
-		}
-		//this.list = (ArrayList<Student>) JSON.parseObject(scanner.nextLine(), ArrayList.class);
-		scanner.close();
-	}
+    public void deserializeFastJSON(String filename) throws IOException {
+        Scanner scanner = new Scanner(new FileReader(filename));
+        this.clear();
+        ArrayList<JSONObject> JSONlist = JSON.parseObject(scanner.nextLine(), ArrayList.class);
+        for (JSONObject st : JSONlist) {
+            this.add(new Student(st.getString("name"), st.getIntValue("age")));
+        }
+        scanner.close();
+    }
 
-	public void add(Student student)
-	{
-		this.list.add(student);
-	}
+    public void add(Student student) {
+        this.list.add(student);
+    }
 }
--- src/com/itschool/Main.java
+++ src/com/itschool/Main.java
@@ -2,58 +2,55 @@ package com.itschool;
 
 import java.io.IOException;
 
-public class Main
-{
+public class Main {
 
-	public static void main(String[] args) throws IOException
-	{
-		Database db = new Database();
-		db.add("Egor", 18);
-		db.add("John", 19);
-		db.add("Bill", 59);
-		db.add(new Student("Null", 2100000000));
-		// System.out.println(db);
+    public static void main(String[] args) throws IOException {
+        final int N = 100000;
+        Database db = new Database();
+        db.add("Egor", 18);
+        db.add("John", 19);
+        db.add("Bill", 59);
+        db.add(new Student("Integer", Integer.MAX_VALUE));
+        System.out.println(db);
 
-		long t1 = System.currentTimeMillis(), t2, t3, t4, t5, t6;
-		for (int i = 0; i < 10; i++) {
-			db.add("John" + Math.random(), (int) (Math.random() * 100));
-		}
-		t2 = System.currentTimeMillis() - t1;
-		db.save("db.txt");
-		db.clear();
-		db.load("db.txt");
+        long timeStart = System.currentTimeMillis(), t1, t2, t3, t4, t5;
+        for (int i = 0; i < N; i++) {
+            db.add("John" + Math.random(), (int) (Math.random() * 100));
+        }
+        t1 = System.currentTimeMillis() - timeStart;
 
-		//System.out.println(db);
-		t3 = System.currentTimeMillis() - t2 - t1;
-		// System.out.println("Current DB: " + db);
-		//// System.out.println("Loaded DB: " + db);
-		db.serialize("db_s.txt");
-		db.clear();
-		db.deserialize("db_s.txt");
-		t4 = System.currentTimeMillis() - t3 - t2 - t1;
+        timeStart = System.currentTimeMillis();
+        db.save("db.txt");
+        db.clear();
+        db.load("db.txt");
+        t2 = System.currentTimeMillis() - timeStart;
 
-		//System.out.println("Loaded deseriaized DB: " + db);
+        timeStart = System.currentTimeMillis();
+        db.serialize("db_s.txt");
+        db.clear();
+        db.deserialize("db_s.txt");
+        t3 = System.currentTimeMillis() - timeStart;
 
-		db.jacksonSerialize("students.json");
-		db.clear();
-		db.jacksonDeserialize("students.json");
-		t5 = System.currentTimeMillis() - t4 - t3 - t2 - t1;
+        timeStart = System.currentTimeMillis();
+        db.jacksonSerialize("students.json");
+        db.clear();
+        db.jacksonDeserialize("students.json");
+        t4 = System.currentTimeMillis() - timeStart;
 
-		// System.out.println(db);
+        timeStart = System.currentTimeMillis();
+        db.serializeFastJSON("db_fastjson.txt");
+        db.clear();
+        db.deserializeFastJSON("db_fastjson.txt");
+        t5 = System.currentTimeMillis() - timeStart;
 
-		db.serializeFastJSON("db_fastjson.txt");
-		db.clear();
-		db.deserializeFastJSON("db_fastjson.txt");
-//		System.out.println("Loaded fastJSON deseriaized DB: " + db);
-		t6 = System.currentTimeMillis() - t5 - t4 - t3 - t2 - t1;
+        System.out.println("ArrayList of " + N + " objects");
+        System.out.println("Initial Data Generation:	" + t1 + " ms");
+        System.out.println("Text format Save/load:		" + t2 + " ms");
+        System.out.println("Java serialization/des:		" + t3 + " ms");
+        System.out.println("Jackson serialization/des:	" + t4 + " ms");
+        System.out.println("FASTJson serialization/des:	" + t5 + " ms");
 
-		System.out.println("Data Generation time:    " + t2);
-		System.out.println("Text Save/load  time:    " + t3);
-		System.out.println("Java serialization time: " + t4);
-		System.out.println("Jackson ser/des time:    " + t5);
-		System.out.println("FASTJson ser/des time:   " + t6);
-/*
-        System.out.println(db.remove(1));
+/*      System.out.println(db.remove(1));
         System.out.println(db);*/
-	}
+    }
 }
